c7e3c6aef6ccfcd7c8937aa184ce355734d0b88a
Girish21 <girishkumarv21@gmail.com>
2022-05-14 15:12:24 +0530

    keyboard subpage with routes

--- src/Pages/keyboard.tsx
+++ src/Pages/keyboard.tsx
@@ -1,3 +1,15 @@
+import { Outlet } from 'react-router-dom'
+import { KeyboardSubnav, PageWrapper } from '../components'
+
 export default function Keyboard() {
-  return null
+  return (
+    <PageWrapper>
+      <div className='col-span-12 flex flex-col pt-2'>
+        <KeyboardSubnav />
+        <div className='grid flex-1 place-content-center'>
+          <Outlet />
+        </div>
+      </div>
+    </PageWrapper>
+  )
 }
