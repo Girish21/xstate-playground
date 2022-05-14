a05b96300a6b5f70a17d48bdedefdd6941afcc69
Girish21 <girishkumarv21@gmail.com>
2022-05-14 15:07:56 +0530

    add prettier tailwind plugin

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false,
+  trailingComma: "all",
+  arrowParens: "avoid",
+  singleQuote: true,
+  jsxSingleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+}
